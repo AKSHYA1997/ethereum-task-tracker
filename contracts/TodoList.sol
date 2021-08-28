fa4a1b8bf9214a426981cba03ab4c35fd3d45dbf
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-08-29 00:04:57 +0530

    :sparkles: added index html and app js file

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,24 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    //Mapping (key => value)
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("This is first task");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount++;
+        //tasks => mappings
+        //Task => struct
+        tasks[taskCount] =Task(taskCount, _content, false);
+    }
 }
