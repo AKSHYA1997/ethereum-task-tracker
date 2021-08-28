7ba05477802714b82604fb5d89dddb2481f5c2cf
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-08-29 01:54:16 +0530

    :sparkles: added task completed function :white_check_mark: added test case for task completed function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -18,6 +18,11 @@ contract TodoList {
         string content,
         bool completed
     );
+    
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
 
     constructor() public {
         createTask("This is first task");
@@ -29,5 +34,12 @@ contract TodoList {
         //Task => struct
         tasks[taskCount] =Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
